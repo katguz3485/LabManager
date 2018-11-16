aead1c62851a5c60521f550b6e0b32209340b8fa
katguz3485 <katarzyna.guzik01@gmail.com>
2018-11-16 09:04:20 +0100

    sqlite3_removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,13 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
