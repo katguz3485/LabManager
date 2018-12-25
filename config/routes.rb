22a1b4c02c71ae81ee16e6fedec1540e68602a3b
katguz3485 <katarzyna.guzik01@gmail.com>
2018-12-25 22:22:33 +0100

    [Add, Fix] categories routes, footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :chemicals
+
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations",
                                    confirmations: "users/confirmations"}
 
