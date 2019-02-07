c10bc1b6d529c8d62feb00b8c8a98939a6d6ca55
katguz3485 <katarzyna.guzik01@gmail.com>
2019-02-07 17:55:03 +0100

    [Add] working cas number validator

--- app/models/chemical.rb
+++ app/models/chemical.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class Chemical < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with CasNumberValidator
+
   belongs_to :category
   has_one :safety_precaution
   has_many :items
   accepts_nested_attributes_for :safety_precaution, allow_destroy: true
-  validates :chemical_name, presence: true, uniqueness: { case_sensitive: false }
-  validates :formula, :cas_number, :molecular_weight, presence: true
+  validates :chemical_name, presence: true, uniqueness: {case_sensitive: false}
+  validates :formula, :molecular_weight, presence: true
+
+  validates_with CasNumberValidator
   mount_uploader :formula_picture, FormulaFileUploader
 
-  attr_accessor  :process_original_version
+  attr_accessor :process_original_version
 end
---
+++ app/validators/cas_number_validator.rb
@@ -0,0 +1,27 @@
+class CasNumberValidator < ActiveModel::Validator
+
+  def validate(record)
+    cpd_cas_number = record.cas_number
+    unless is_valid?(cpd_cas_number)
+      record.errors.add(:cas_number, "is invalid")
+    end
+  end
+
+  private
+
+  def is_valid?(cpd_cas_number)
+    check_cas_number(cpd_cas_number)
+  end
+
+  def check_cas_number(cas_number)
+    return false unless cas_number && cas_number.match(/[0-9]{2,7}-[0-9]{2}-[0-9]/)
+    check_digit = cas_number[-1, 1].to_i
+    sum = 0
+    cas_number.reverse.scan(/[0-9]/).each_with_index do |digit, i|
+      sum = sum + digit.to_i * i
+    end
+    check_digit == sum.remainder(10)
+  end
+
+
+end
