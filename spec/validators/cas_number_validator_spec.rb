2a9fbcfc59faa4cd360bf0efa97c96fb47fb5c54
katguz3485 <katarzyna.guzik01@gmail.com>
2019-03-03 22:36:55 +0100

    Update cas_number_validator_spec.rb

--- spec/validators/cas_number_validator_spec.rb
+++ spec/validators/cas_number_validator_spec.rb
@@ -1,31 +1,4 @@
 require "rails_helper"
-#   def validate(record)
-#     @errors = []
-#     @record = record
-#
-#     has_proper_format?
-#     check_digit_evaluate?
-#
-#     record.errors.add(:cas_number, errors) if errors.any?
-#   end
-#
-#   private
-#
-#   def has_proper_format?
-#     errors << "Enter properly formatter CAS" unless record.cas_number&.match(/[0-9]{2,7}-[0-9]{2}-[0-9]/)
-#   end
-#
-#   def check_digit_evaluate?
-#
-#     check_digit = record.cas_number[-1, 1].to_i
-#     sum = 0
-#     record.cas_number.to_s.reverse.scan(/[0-9]/).each_with_index do |digit, i|
-#       sum = sum + digit.to_i * i
-#     end
-#     errors << "CAS is invalid enter the proper CAS" unless check_digit == sum.remainder(10)
-#   end
-# end
-
 describe CasNumberValidator do
   subject {Validatable.new(cas_number: cas_number)}
 
