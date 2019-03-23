61795c769979c4166e65e52ad86eddd061bc2e1b
katguz3485 <katarzyna.guzik01@gmail.com>
2019-03-23 23:59:41 +0100

    remove commented line

--- app/services/chemical_services/pub_chem_service_picture.rb
+++ app/services/chemical_services/pub_chem_service_picture.rb
@@ -30,35 +30,6 @@ module ChemicalServices
       chemical.assign_attributes(picture_url: picture)
       chemical.save(validate: false)
 
-      binding.pry
-
     end
-
-    # def update_picture_url
-    #
-    # require 'open-uri'
-    #
-    # class GetAlpacaPictures
-    #   API_KEY = Rails.application.config.google_custom_search_api_key
-    #   ENGINE_ID = Rails.application.config.google_custom_search_engine_id
-    #   URL = 'https://www.googleapis.com/customsearch/v1?key=' + API_KEY + '&cx=' + ENGINE_ID +
-    #         '&searchType=image&q=alpaca&imgSize=medium&start='
-    #
-    #   def call
-    #     address_with_random_index = URL + (1..90).to_a.sample.to_s
-    #     begin
-    #       open(address_with_random_index).read
-    #     rescue OpenURI::HTTPError => error
-    #       return nil
-    #     end
-    #   end
-    # end
-    #
-    #
-    #
-    #
-    # end
-
-
   end
 end
